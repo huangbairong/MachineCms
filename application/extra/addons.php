d849efa100e454019ba0d577775ca4c7e34cd3e3
黄佰荣 <huangbairong@quseit.com>
2025-09-17 15:57:04 +0800

    安装fastadmin - ldcms 插件

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -2,8 +2,25 @@
 
 return [
     'autoload' => false,
-    'hooks' => [],
-    'route' => [],
+    'hooks' => [
+        'upgrade' => [
+            'ldcms',
+        ],
+        'app_init' => [
+            'ldcms',
+        ],
+        'config_init' => [
+            'ldcms',
+        ],
+    ],
+    'route' => [
+        '/$' => 'ldcms/index/index',
+        '/search' => 'ldcms/search/index',
+        '/sitemap.xml' => 'ldcms/sitemap/index',
+        '/tag/[:tag]$' => 'ldcms/tag/index',
+        '/[:category]$' => 'ldcms/lists/index',
+        '/[:category]/[:id]$' => 'ldcms/detail/index',
+    ],
     'priority' => [],
     'domain' => '',
 ];
--- xxooo.md
+++ xxooo.md
@@ -1,18 +1,13 @@
 FTP账号资料
 用户：
-machine_cyh809_cn
+mechine_cyh809_cn
 密码：
 HxKntFMKC5SMx9ND
 数据库账号资料
 数据库名：
-machine
+mechine
 用户：
-machine
+mechine
 密码：
-A1pNwipRrXxKwJ6a
+abc123456
 网站添加成功！
-
-## 站点
-  - 前台地址:http://machine.cyh809.cn/ 
-  - 后台地址：http://machine.cyh809.cn/dheYInijcR.php/index/login
-  - 登录账号：admin/abc123456
