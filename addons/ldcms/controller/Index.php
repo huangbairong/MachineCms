25a64e2fae1d378c56652ca4c92a7941c75d9e08
yibai <yibai@Bai>
2025-09-18 01:19:50 +0800

    站点02 /index.php/addons/ldcms/index/index02

--- addons/ldcms/controller/Index.php
+++ addons/ldcms/controller/Index.php
@@ -18,4 +18,16 @@ class Index extends Base
         return $this->view->fetch('/index');
     }
 
+    public function index02()
+    {
+        $index_title=$this->addonConfig['index_title'];
+        if(!empty($index_title)){
+            $index_title=$this->view->display($index_title);
+            $this->siteConfig['sitetitle']=$index_title;
+            $this->assign('ld', $this->siteConfig);
+        }
+
+        return $this->view->fetch('/index02');
+    }
+
 }
